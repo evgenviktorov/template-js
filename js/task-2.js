8f95a0142ee1a9ac6d245c92c46832c604acd1a8
EV <evgenviktorov88@gmail.com>
2025-03-22 20:15:40 +0200

    initial

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,3 @@
 console.log('-- task-2 --');
 
-function makeArray(firstArray, secondArray, maxLength) {
-  const fullArray = firstArray.concat(secondArray);
-  const lenFullArray = fullArray.length;
-  if (lenFullArray > maxLength) {
-    return fullArray.splice(0, maxLength);
-  } else return fullArray;
-}
-
 // ==============
-
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
